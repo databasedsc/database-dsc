bd547caea525fb1d62379ce40de5eb3ecaf59216
Bengt Hammarlund and Luis Urraca <pair+bengt+luis@pivotal.io>
2016-02-18 17:07:36 +0000

    Adding seeds for company

--- server/dsc-db-be/db/seeds.rb
+++ server/dsc-db-be/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Company.create(
+  [
+    {
+      "name": "Mustard",
+      "logo": "https://crunchbase-production-res.cloudinary.com/image/upload/c_pad,w_250,h_140/v1455114908/tmpxdogqv3vysbaww9g3.png",
+      "short_description": "The on demand staffing network"
+    },
+    {
+      "name": "Boxever",
+      "logo": "https://crunchbase-production-res.cloudinary.com/image/upload/c_pad,w_250,h_140/v1407341479/e8h5rkwhe0cnfsmwhppf.jpg",
+      "short_description": "The on demand staffing network"
+    },
+    {
+      "name": "Logentries",
+      "logo": "https://crunchbase-production-res.cloudinary.com/image/upload/c_pad,w_250,h_140/v1415669358/cfx78b8ybfao4orxttaa.png",
+      "short_description": "Real-time log management and analytics"
+    }
+  ]
+)
