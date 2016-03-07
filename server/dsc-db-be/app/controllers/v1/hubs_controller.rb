318dcbbbc5dcdc32e5da090a00fdc3eca160f785
Luis Urraca <pair+luis@pivotal.io>
2016-03-07 11:35:22 +0000

    Fixes issue in hubs index controller action, filter by hubType.

--- server/dsc-db-be/app/controllers/v1/hubs_controller.rb
+++ server/dsc-db-be/app/controllers/v1/hubs_controller.rb
@@ -1,4 +1,6 @@
 class V1::HubsController < ApplicationController
+  before_action :hub_types_parse, only: :index
+
   def index
     if params[:searchText]
       hubs = Hub.search(params[:searchText])
@@ -7,8 +9,13 @@ class V1::HubsController < ApplicationController
     end
 
     hubs = hubs.application_deadline(params[:applicationDeadlines]) if params[:applicationDeadlines]
-    hubs = hubs.hub_type(params[:hubType]) if params[:hubType]
+    hubs = hubs.hub_type(params[:hubType]) if params[:hubType].present?
 
     render json: hubs, status: 200
+  end
+
+
+  def hub_types_parse
+    params[:hubType] = JSON.parse(params[:hubType]).select { |k, v| v }.keys.join(' ') if params[:hubType]
   end
 end
