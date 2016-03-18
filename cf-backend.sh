7d9850a684557d049c19b4ab148b21b7a641e516
Luis Urraca <pair+luis@pivotal.io>
2016-03-18 16:54:47 +0000

    Add a new environment, integration.

--- cf-backend.sh
+++ cf-backend.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -e
-
 cd server/dsc-db-be
+echo 'CF Download start'
 wget https://s3.amazonaws.com/go-cli/releases/v6.12.4/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb
 echo 'CF DONE - STARTING DEPLOY'
 echo $CF_EMAIL $CF_PWD
