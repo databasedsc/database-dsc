a78b2e2a06b994399ad7cdaa485ae41a58fe5bf8
Brian Kenny <brian@minicorp.ie>
2016-04-23 13:06:50 +0100

    Complete homepage tiles

--- src/app/modules/home/home.component.js
+++ src/app/modules/home/home.component.js
@@ -6,7 +6,7 @@
       templateUrl: 'app/modules/home/home.html',
       controller: 'HomeController'
     })
-    .controller('HomeController', function ($scope, searchCompaniesService) {
+    .controller('HomeController', function ($scope, searchCompaniesService, searchInvestorsService, searchHubsService, searchMultinationalsService) {
       var controller = this;
       this.searchCompaniesService = searchCompaniesService;
 
@@ -16,6 +16,9 @@
 
       $scope.$watch('currentPage', function () {
         controller.gatherCompanies();
+        controller.gatherInvestors();
+        controller.gatherHubs();
+        controller.gatherMtns();
       }, true);
 
       function getPaginationDetails() {
@@ -27,11 +30,31 @@
 
       this.gatherCompanies = function() {
         searchCompaniesService.getCompanies({searchText: this.query}, getPaginationDetails()).then(function(companies) {
-          controller.results = companies.data;
-          controller.totalItems = companies.headers('Total')
+          controller.companyResults = companies.data;
+          controller.totalCompanyItems = companies.headers('Total')
         });
       };
 
+      this.gatherInvestors = function() {
+        searchInvestorsService.get({searchText: this.query}, getPaginationDetails()).then(function(investors) {
+          controller.investorResults = investors.data;
+          controller.totalInvestorItems = investors.headers('Total');
+        });
+      };
+
+      this.gatherHubs = function() {
+        searchHubsService.get({searchText: controller.query}, getPaginationDetails()).then(function(hubs) {
+          controller.hubResults = hubs.data;
+          controller.totalHubItems = hubs.headers('Total');
+        })
+      }
+
+      this.gatherMtns = function() {
+        searchMultinationalsService.get({searchText: controller.query}, getPaginationDetails()).then(function(multinationals) {
+          controller.mtnsResults = multinationals.data;
+          controller.totalMtnsItems = multinationals.headers('Total');
+        })
+      }
 
     });
 })();
