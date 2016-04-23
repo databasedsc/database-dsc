0996a0ee89b15520361c65102360f0c9f25ad654
Brian Kenny <brian@minicorp.ie>
2016-04-23 12:54:44 +0100

    Add recent companies to homepage

--- src/app/modules/home/home.component.js
+++ src/app/modules/home/home.component.js
@@ -6,7 +6,32 @@
       templateUrl: 'app/modules/home/home.html',
       controller: 'HomeController'
     })
-    .controller('HomeController', function () {
-      this.message = 'DSC Initial message';
+    .controller('HomeController', function ($scope, searchCompaniesService) {
+      var controller = this;
+      this.searchCompaniesService = searchCompaniesService;
+
+      $scope.currentPage = 1;
+      $scope.perPage = 4;
+      this.query = "";
+
+      $scope.$watch('currentPage', function () {
+        controller.gatherCompanies();
+      }, true);
+
+      function getPaginationDetails() {
+        return {
+          currentPage: $scope.currentPage,
+          perPage: $scope.perPage
+        }
+      }
+
+      this.gatherCompanies = function() {
+        searchCompaniesService.getCompanies({searchText: this.query}, getPaginationDetails()).then(function(companies) {
+          controller.results = companies.data;
+          controller.totalItems = companies.headers('Total')
+        });
+      };
+
+
     });
 })();
