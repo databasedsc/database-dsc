352448f014fef0d71504fa14e42ff5a9d2077289
Brian Kenny <brian@minicorp.ie>
2016-05-24 15:37:03 +0100

    Ensure admin

--- server/dsc-db-be/app/controllers/v1/admin/user_entity_claims_controller.rb
+++ server/dsc-db-be/app/controllers/v1/admin/user_entity_claims_controller.rb
@@ -2,12 +2,19 @@ module V1
   module Admin
     class UserEntityClaimsController < ApplicationController
       before_action :authenticate
-      # before_action :is_user_admin
+      before_action :is_user_admin
 
       def index
         user_entity_claims = UserEntityClaim.all
 
         render json: user_entity_claims
+      end
+
+      def is_user_admin
+        if current_user.user_type != "admin"
+          render json: :nothing, status: 401
+          return
+        end
       end
 
       def update
